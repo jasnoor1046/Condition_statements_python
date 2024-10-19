10acec5121ba61dd6dfd74a692990b4bf84d089a
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-19 14:06:36 +0530

    added program6.py

---
+++ program6.py
@@ -0,0 +1,19 @@
+marks= int(input("Enter the marks of the student"))
+
+if marks >= 90:
+    print("The grade is A+")
+
+elif marks >80:
+    print("The grade is A")
+
+elif marks >=70:
+    print("The grade is B+")
+
+elif marks >=60:
+    print("The grade is B")
+
+elif marks >=50:
+    print("The grade is C")
+
+else:
+    print("Student got fail")
