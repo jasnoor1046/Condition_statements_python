ceaea60f7dfb190f6debceb37eedbc29a9e8a3fe
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-19 21:59:15 +0530

    added program8.py

---
+++ program8.py
@@ -0,0 +1,8 @@
+ch= input("Enter a character: ")
+
+if(ch=='A' or ch=='a' or ch== 'e' or ch=='E' or ch=='i' or ch=='I' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "is a vowel")
+
+else:
+    print("It is a consonant")
+    
