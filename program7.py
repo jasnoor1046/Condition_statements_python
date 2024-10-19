44c01431b73e7874f2063b7d377bd853a727f535
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-19 21:58:34 +0530

    added program7.py

---
+++ program7.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number"))
+
+if n%5==0 and n%11==0:
+    print("The given number is divisible by both 5 and 11")
+
+else:
+    print("The number is not divisible by any of the above")
