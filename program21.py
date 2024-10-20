27365bbab47629043cfcace301976c9fe54926f5
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-20 13:30:18 +0530

    added program21.py

---
+++ program21.py
@@ -0,0 +1,11 @@
+# to check the password enetered by the user
+
+current_password= 'python123'
+
+password= input("Enter the password: ")
+
+if password== current_password:
+    print("Acess is granted")
+
+else:
+    print("acess is not granted")
