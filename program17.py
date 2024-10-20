f3e93a19320641e39801b7aad55f4bcf8f55bd02
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-20 13:27:37 +0530

    added program17.py

---
+++ program17.py
@@ -0,0 +1,11 @@
+T= int(input("Enter the temperature: ")) # to check the temperature
+
+if T>30:
+    print("It's Hot")
+
+elif T>=20 and T<=30:
+    print("It's warm")
+
+else:
+    print("It's cold")
+
