4cd96435c5c6432af1b71cedefef4db826d229b5
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-20 13:26:09 +0530

    added program15.py

---
+++ program15.py
@@ -0,0 +1,13 @@
+n1= int(input("enter the first number:  "))
+n2= int(input("enter the second number: "))
+
+if n1>n2:
+    print("First number is greater than the second")
+
+elif n1==n2:
+    print("Both are equal")
+
+else:
+    print("second one is greater")
+    
+
